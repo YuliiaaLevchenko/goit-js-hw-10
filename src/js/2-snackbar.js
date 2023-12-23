4755127e5c63e732bd6c409b0d5878dfa01fe163
YuliiaaLevchenko <141173167+YuliiaaLevchenko@users.noreply.github.com>
2023-12-23 18:54:31 +0200

    snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,28 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delay = parseInt(this.elements['delay'].value, 10);
+  const state = this.elements['state'].value;
+
+  const notificationPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  notificationPromise
+    .then((delay) => {
+      iziToast.success({ message: `Fulfilled promise in ${delay}ms` });
+    })
+    .catch((delay) => {
+      iziToast.error({ message: `Rejected promise in ${delay}ms` });
+    });
+});
