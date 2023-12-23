f79bf57e18c227876d1c20d79f6ce4ec49215a99
YuliiaaLevchenko <141173167+YuliiaaLevchenko@users.noreply.github.com>
2023-12-23 18:34:25 +0200

    timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,5 +1,7 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 const options = {
     enableTime: true,
@@ -10,6 +12,10 @@ const options = {
       console.log(selectedDates[0]);
     },
   };
+
+  function addLeadingZero(value) {
+    return toString(value).padStart(2, '0');
+  }
 
   function convertMs(ms) {
     // Number of milliseconds per unit of time
@@ -33,3 +39,67 @@ const options = {
   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
   console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+  let countdownInterval;
+
+  function updateTimerDisplay(timeLeft) {
+    const { days, hours, minutes, seconds } = convertMs(timeLeft);
+    const formattedTime = `$
+    {addLeadingZero(days)}:$
+    {addLeadingZero(hours)}:$
+    {addLeadingZero(minutes)}:$
+    {addLeadingZero(seconds)}`;
+    
+    const startButton = document.querySelector('[data-start]');
+    if (startButton) {
+        startButton.innerText = formattedTime;}
+    }
+    
+  function startCountdown(targetDate) {
+    clearInterval(countdownInterval);
+
+    countdownInterval = setInterval(function() {
+        const currentDate = new Date().getTime();
+        const timeLeft = targetDate - currentDate;
+    
+        if (timeLeft <= 0) {
+          clearInterval(countdownInterval);
+          updateTimerDisplay(0);
+          iziToast.success({ message: 'Countdown finished!' });
+          const startButton = document.querySelector('[data-start]');
+          if (startButton) {
+            startButton.disabled = true;
+          }
+        } else {
+          updateTimerDisplay(timeLeft);
+        }
+      }, 1000);
+    }
+
+    let flatpickrInstance;
+
+    flatpickr('#datetime-picker', {
+      minDate: 'today',
+      onChange: function(selectedDates, dateStr, instance) {
+        flatpickrInstance = instance;
+        const selectedDate = selectedDates.length > 0 ? selectedDates[0] : null;
+        const currentDate = new Date();
+        const startButton = document.querySelector('[data-start]');
+      
+        if (startButton && selectedDate > currentDate) {
+          startButton.disabled = false;
+        } else if (startButton) {
+          startButton.disabled = true;
+          iziToast.error({ message: 'Please choose a date in the future' });
+        }
+      }
+    });
+    
+    document.querySelector('[data-start]').addEventListener('click', function() {
+      const selectedDate = flatpickrInstance?.selectedDates?.[0];
+      if (selectedDate) {
+        startCountdown(selectedDate.getTime());
+        this.disabled = true;
+      
+      }
+    });
